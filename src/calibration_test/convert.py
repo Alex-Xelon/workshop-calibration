e7bada7dbbb9784b74103c8e84c73d7a0818bd6a
Alexandre Lemiere <alexandre.lemiere11@gmail.com>
2025-07-18 11:52:47 +0200

    Update data loading in convert.py to use TSV format

--- src/calibration_test/convert.py
+++ src/calibration_test/convert.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import arff
 
+
 # Load the CSV
-df = pd.read_csv("data/diabetes.csv")
+df = pd.read_csv("hf://datasets/mihaicata/diabetes/all_data_processed.tsv", sep="\t")
+
+inputs_expanded = (
+    df["inputs"]
+    .astype(str)
+    .str.replace("'", "")
+    .str.replace(" ", "")
+    .str.replace("[", "")
+    .str.replace("]", "")
+    .str.split(",", expand=True)
+)
+
+inputs_expanded.columns = [f"input_{i}" for i in range(inputs_expanded.shape[1])]
+inputs_expanded = inputs_expanded.dropna(axis=1).apply(pd.to_numeric)
+
+df = pd.concat([inputs_expanded, df["label"].reset_index(drop=True)], axis=1)
+print(df.head(10))
 
 # Define the attributes
 attributes = []
@@ -15,6 +32,6 @@ data = df.values.tolist()
 arff.dump(
     "data/dataset_simpleclass.arff",
     data,
-    relation="Diabetes",
+    relation="Diabete",
     names=[attr[0] for attr in attributes],
 )
